2371bfe784407eb5e306b04342d68b3fbcdec1a3
Tomas Coufal <coufal.tom@gmail.com>
2017-04-26 17:39:11 +0200

    Add PRL project3 documentation

--- PRL/project3/test.sh
+++ PRL/project3/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Compute amount of used processors
 mat1=$(head -n1 mat1)
 mat2=$(head -n1 mat2)
 
@@ -11,6 +12,7 @@ cat /etc/*-release | grep -q "fedora" && PATH=$PATH:/usr/lib64/openmpi/bin/
 # If running on CentOS add prefix
 cat /etc/*-release | grep -q "centos" && PREFIX="--prefix /usr/local/share/OpenMPI"
 
+# Compile and run
 mpic++ $PREFIX $DEFINE -o mm mm.cpp -std=c++0x
 mpirun $PREFIX -np $cpus mm
 rm -f mm
