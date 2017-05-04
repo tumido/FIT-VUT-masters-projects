1571002f495cb63015b915ec583221f01f350584
Tomas Coufal <coufal.tom@gmail.com>
2017-05-05 00:52:00 +0200

    KRY project2 basic verify

--- KRY/project2/validate_certs.cpp
+++ KRY/project2/validate_certs.cpp
@@ -10,23 +10,34 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 #include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 // SSL Connection
 #include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define BASE_HOST ".minotaur.fi.muni.cz"
 #define PORT "443"
 #define SERVER_COUNT 100
+#define LOCAL_CERT "crocs-ca.pem"
 
 #define eprintf_(host, msg, retval) {\
-  fprintf(stderr, "\x1B[31m[%s] %s\033[0m\n", host.c_str(), msg);\
+  fprintf(stderr, "\x1B[31mERROR [%s] \033[0m%s\n", host.c_str(), msg);\
   return retval;\
 }
 #define eprintf(host, msg) eprintf_(host, msg, EXIT_FAILURE)
+#define success_printf(host, msg) \
+  fprintf(stderr, "\x1B[32mOK    [%s] \033[0m%s\n", host.c_str(), msg);
+
+#define oprintf(host,cat, msg) printf("%s,%c,%s\n", host.c_str(), cat, msg)
+
+#define in_category(arr, x) any_of(begin(arr), end(arr), [&](long i) { return i == x; })
+
+typedef struct {
+  X509 * cert;
+  int errno;
+} SSL_STATUS;
 
 using namespace std;
 
@@ -42,76 +53,91 @@ vector<string> get_hostnames() {
 
   return hostnames;
 }
+//
+// void verify_cert(const string host, X509 * cert) {
+//   // cout << host <<endl;
+//
+//   BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
+//
+//   //Subject
+//   BIO_printf(bio_out,"Subject: ");
+//   X509_NAME_print(bio_out,X509_get_subject_name(cert),0);
+//   BIO_printf(bio_out,"\n");
+//
+//   //Issuer
+//   BIO_printf(bio_out,"Issuer: ");
+//   X509_NAME_print(bio_out,X509_get_issuer_name(cert),0);
+//   BIO_printf(bio_out,"\n");
+//
+//   //Public Key
+//   EVP_PKEY *pkey=X509_get_pubkey(cert);
+//   EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
+//   EVP_PKEY_free(pkey);
+//   //
+//   // //Signature
+//   X509_signature_print(bio_out, cert->sig_alg, cert->signature);
+//   BIO_printf(bio_out,"\n");
+//
+//   cout << endl;
+//   BIO_free(bio_out);
+// }
 
-void verify_cert(const string host, X509 * cert) {
-  // cout << host <<endl;
-
-  BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
-
-  //Subject
-  BIO_printf(bio_out,"Subject: ");
-  X509_NAME_print(bio_out,X509_get_subject_name(cert),0);
-  BIO_printf(bio_out,"\n");
-
-  //Issuer
-  // BIO_printf(bio_out,"Issuer: ");
-  X509_NAME_print(bio_out,X509_get_issuer_name(cert),0);
-  // BIO_printf(bio_out,"\n");
-
-  //Public Key
-  EVP_PKEY *pkey=X509_get_pubkey(cert);
-  EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
-  EVP_PKEY_free(pkey);
-  //
-  // //Signature
-  X509_signature_print(bio_out, cert->sig_alg, cert->signature);
-  // BIO_printf(bio_out,"\n");
-
-  cout << endl;
-  BIO_free(bio_out);
-}
-
-int connect_to_host(const string host) {
-  struct addrinfo hints, * res;
-	int status, sock;
-
-	//clear hints
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-
-	status = getaddrinfo(host.c_str(), PORT, &hints, &res);
-	if(status != 0) eprintf(host, "Unable to get ADDR info");
-
-	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if(sock < 0) eprintf(host, "Error when creating socket");
-
-	status = connect(sock, res->ai_addr, res->ai_addrlen);
-	if(status < 0) eprintf(host, "Unable to open TCP connection");
-
-	freeaddrinfo(res);
-  return sock;
-}
-
-X509 * fetch_certificate(const string host, int sock) {
-  const SSL_METHOD *meth = SSLv23_client_method();
+int setup_ssl(SSL_CTX ** ctx) {
+  const SSL_METHOD* meth = SSLv23_client_method();
   OpenSSL_add_ssl_algorithms();
 
-  SSL_CTX *ctx = SSL_CTX_new(meth);
-  SSL *ssl = SSL_new(ctx);
-  BIO *sbio = BIO_new_socket(sock,BIO_NOCLOSE);
+	*ctx = SSL_CTX_new(meth);
+  if (!*ctx) eprintf( string("GLOBAL"), "Unable to setup SSL_CTX");
+	SSL_CTX_set_verify_depth(*ctx, 4);
 
-  SSL_set_tlsext_host_name(ssl, host.c_str());
+	SSL_CTX_set_options(*ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
+  const char *dir = getenv(X509_get_default_cert_dir_env());
+  if (!dir) dir = X509_get_default_cert_dir();
+	SSL_CTX_load_verify_locations(*ctx, NULL, dir);
+  SSL_CTX_load_verify_locations(*ctx, LOCAL_CERT, NULL);
+  return EXIT_SUCCESS;
+}
 
-  SSL_set_bio(ssl,sbio,sbio);
-  if (SSL_connect(ssl) <= 0)
-    eprintf_(host, "Unable to establish SSL", NULL);
 
-  X509 *peer = SSL_get_peer_certificate(ssl);
+int connect_to_host(const string host, SSL_CTX * ctx, SSL ** ssl) {
+	BIO * bio = BIO_new_ssl_connect(ctx);
+  if (!bio) eprintf(host, "Unable to setup BIO");
 
-  SSL_CTX_free(ctx);
-  return peer;
+	BIO_set_conn_hostname(bio, host.c_str());
+  BIO_set_conn_port(bio, PORT);
+
+	BIO_get_ssl(bio, ssl);
+  if (!ssl) eprintf(host, "Unable to setup SSL");
+
+	SSL_set_mode(*ssl, SSL_MODE_AUTO_RETRY);
+	SSL_set_tlsext_host_name(*ssl, host.c_str());
+
+	BIO_new_fp(stdout, BIO_NOCLOSE);
+	long res = BIO_do_connect(bio);
+  if (res <= 0) {ERR_print_errors_fp(stderr); eprintf(host, "Unable to connect");}
+	res = BIO_do_handshake(bio);
+  if (res <= 0) {ERR_print_errors_fp(stderr); eprintf(host, "Unable to handshake");}
+  return EXIT_SUCCESS;
+}
+
+int verify_certificate(const string host, SSL * ssl) {
+  long cat_1[] = {X509_V_OK};
+  long cat_2[] = {X509_V_ERR_CERT_HAS_EXPIRED};
+  // long cat_3[] = {};
+
+  // X509 *cert = SSL_get_peer_certificate(ssl);
+  // STACK_OF(X509) *sk = SSL_get_peer_cert_chain(ssl);
+
+  long res = SSL_get_verify_result(ssl);
+
+  char cat = '0';
+	if (in_category(cat_1, res)) cat = '1';
+  else if (in_category(cat_2, res)) cat = '2';
+  // else if (in_category(cat_3, res)) cat = '3';
+  else cat = '4';
+  oprintf(host, cat, X509_verify_cert_error_string(res));
+
+  return EXIT_SUCCESS;
 }
 
 int main() {
@@ -119,14 +145,12 @@ int main() {
   SSL_load_error_strings();
   SSL_library_init();
 
-  int sock;
-  X509 * cert;
+  SSL_CTX * ctx;
+  SSL * ssl;
+  setup_ssl(&ctx);
   for (vector<string>::iterator it = hostnames.begin(); it < hostnames.end(); it++) {
-    sock = connect_to_host(*it);
-    cert = fetch_certificate(*it, sock);
-    verify_cert(*it, cert);
-    X509_free(cert);
-    close(sock);
+    connect_to_host(*it, ctx, &ssl);
+    verify_certificate(*it, ssl);
   }
   return EXIT_SUCCESS;
 }
