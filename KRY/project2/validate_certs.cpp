4a0bb31d8ff4f0db3b867f2d14e0a3f934d9041a
Tomas Coufal <coufal.tom@gmail.com>
2017-05-05 01:36:41 +0200

    KRY project2 cleanup

--- KRY/project2/validate_certs.cpp
+++ KRY/project2/validate_certs.cpp
@@ -53,34 +53,6 @@ vector<string> get_hostnames() {
 
   return hostnames;
 }
-//
-// void verify_cert(const string host, X509 * cert) {
-//   // cout << host <<endl;
-//
-//   BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
-//
-//   //Subject
-//   BIO_printf(bio_out,"Subject: ");
-//   X509_NAME_print(bio_out,X509_get_subject_name(cert),0);
-//   BIO_printf(bio_out,"\n");
-//
-//   //Issuer
-//   BIO_printf(bio_out,"Issuer: ");
-//   X509_NAME_print(bio_out,X509_get_issuer_name(cert),0);
-//   BIO_printf(bio_out,"\n");
-//
-//   //Public Key
-//   EVP_PKEY *pkey=X509_get_pubkey(cert);
-//   EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
-//   EVP_PKEY_free(pkey);
-//   //
-//   // //Signature
-//   X509_signature_print(bio_out, cert->sig_alg, cert->signature);
-//   BIO_printf(bio_out,"\n");
-//
-//   cout << endl;
-//   BIO_free(bio_out);
-// }
 
 int setup_ssl(SSL_CTX ** ctx) {
   const SSL_METHOD* meth = SSLv23_client_method();
