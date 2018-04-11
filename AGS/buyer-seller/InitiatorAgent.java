ea829c68e9c33097c2a479c4583dfcb0c61e0754
Tomas Coufal <coufal.tom@gmail.com>
2018-04-11 17:38:40 +0200

    AGS buyer-seller preparation

--- AGS/buyer-seller/InitiatorAgent.java
+++ AGS/buyer-seller/InitiatorAgent.java
@@ -1,23 +1,117 @@
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.*;
 
 public class InitiatorAgent extends Agent {
-  protected void setup() { 
-    System.out.println("Initiator " + getAID().getName() + " is ready."); 
+  protected int TIMEOUT = 10000;
+  protected Date timeout_stamp;
+  protected List<ACLMessage> proposals = new ArrayList<ACLMessage>();
+
+  protected void setup() {
+    System.out.println("Initiator " + getAID().getName() + ": ready");
     SequentialBehaviour seq = new SequentialBehaviour();
+
+    // Initial delay on the initiator side
     seq.addSubBehaviour(new WakerBehaviour( this, 1000 ) {
         protected void handleElapsedTimeout() {
-         System.out.println( "Initiator " + getAID().getName() + " waited long enough." );
+         System.out.println( "Initiator " + getAID().getName() + ": waited long enough" );
         }
     });
+    // Send out a message that initiator is accepting proposals
     seq.addSubBehaviour(new AskForProposalBehaviour());
+    // Collect proposals form participants
+    seq.addSubBehaviour(new WaitForProposalsBehaviour());
+    // Accept the best proposal
+    seq.addSubBehaviour(new AcceptProposalBehaviour());
+    // Wait for a complete project
+    seq.addSubBehaviour(new WaitForProjectDoneBehaviour());
+
     addBehaviour(seq);
-  } 
+  }
 }
 
-class AskForProposalBehaviour extends OneShotBehaviour{
+class AskForProposalBehaviour extends OneShotBehaviour {
   public void action() {
-    
+    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+
+    // Search and collect all participant services
+    DFAgentDescription template = new DFAgentDescription();
+    DFAgentDescription[] results = DFService.search(myAgent, template, null);
+    for (DFAgentDescription agent : results) cfp.addReceiver(agent.getName());
+
+    // Set timeout
+    cfp.setContent(myAgent.TIMEOUT);
+    myAgent.timeout_stamp = new Date(System.currentTimeMillis() + myAgent.TIMEOUT);
+
+    // Distribute proposal request
+    myAgent.send(cfp);
+
+    System.out.println("Initiator " + myAgent.getAID().getName() + ": Ask for proposal sent");
+  }
+}
+
+class WaitForProposalsBehaviour extends CyclicBehaviour {
+  public void action() {
+    // Listen for message
+    ACLMessage msg = myAgent.receive();
+    if (msg == null) {
+      block();
+      return;
+    }
+
+    // If message contains a proposal, collect it
+    if (msg.getPerformative == ACLMessage.PROPOSE) {
+      System.out.println("Initiator " + myAgent.getAID().getName() + ": Received proposal from " + msg.getSender().getName());
+      proposals.add(msg);
+    }
+  }
+
+  public boolean done() {
+    // Stop when timeout is reached
+    System.out.println("Initiator " + myAgent.getAID().getName() + ": No longer accepting proposals");
+    return myAgent.timeout_stamp < new Date();
+  }
+}
+
+class AcceptProposalBehaviour extends OneShotBehaviour {
+  public void action() {
+    // Select the best offer
+    ACLMessage accept_offer = proposals.min(Comparator.comparing(ACLMessage::getContent));
+    if (!accept_offer) return;
+
+    // Accept the selected proposal
+    ACLMessage accept_msg = accept_offer.createReply();
+    accept_msg.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+    accept_msg.send();
+    System.out.println("Initiator " + myAgent.getAID().getName() + ": Accepted proposal from " + accept_offer.getSender().getName());
+
+    // Distribute a reject message to all other services
+    ACLMessage reject_msg = new ACLMessage(ACLMessage.REJECT_PROPOSAL);
+    for (ACLMessage reject_offer: proposals) {
+      if (reject_offer != accept_offer)
+        reject_msg.addReceiver(reject_offer.getSender());
+    }
+    reject_msg.send();
+
+    System.out.println("Initiator " + myAgent.getAID().getName() + ": Rejected offers from others");
+  }
+}
+
+class WaitForProjectDoneBehaviour extends CyclicBehaviour {
+  public void action() {
+    // Listen for message
+    ACLMessage msg = myAgent.receive();
+    if (msg == null) {
+      block();
+      return;
+    }
+
+    // If message contains a information about complete project, collect it
+    if (msg.getPerformative == ACLMessage.INFORM) {
+      System.out.println("Initiator " + myAgent.getAID().getName() + ": Received complated project with message: " + msg.getContent());
+      System.out.println("Initiator " + myAgent.getAID().getName() + ": Exiting");
+      myAgent.doDelete();
+    }
   }
 }
--- AGS/buyer-seller/ParticipantAgent.java
+++ AGS/buyer-seller/ParticipantAgent.java
@@ -1,4 +1,4 @@
-import jade.core.Agent; 
+import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -7,14 +7,15 @@ import jade.domain.FIPAException;
 import jade.domain.FIPANames;
 import jade.lang.acl.*;
 
-public class ParticipantAgent extends Agent { 
+public class ParticipantAgent extends Agent {
   protected void setup() {
+    System.out.println("Initiator " + getAID().getName() + ": ready");
     try {
         DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(getAID());
         ServiceDescription sd = new ServiceDescription();
         sd.setName(getAID().getName());
-  	sd.setType("ags-participant-type");
+  	    sd.setType("ags-participant-type");
         sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
         dfd.addServices(sd);
         DFService.register(this, dfd);
@@ -22,23 +23,84 @@ public class ParticipantAgent extends Agent {
     catch (FIPAException fe) {
         fe.printStackTrace();
     }
-    
-    System.out.println("Participant " + getAID().getName() + " has created a service."); 
+
+    System.out.println("Participant " + getAID().getName() + ": Service created");
     addBehaviour(new WaitAndRespondBehaviour());
-  } 
+  }
 }
 
 class WaitAndRespondBehaviour extends CyclicBehaviour{
   public void action() {
-    ACLMessage msg = myAgent.receive();  
-    if (msg == null){ 
+    ACLMessage msg = myAgent.receive();
+    if (msg == null) {
       block();
+      return;
     }
-    else{
-      System.out.println("Participant " + myAgent.getAID().getName() + " received message.");
+
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Message received");
+    switch (msg.getPerformative()) {
+      case ACLMessage.CFP:
+        respond_to_cfp(msg); break;
+      case ACLMessage.ACCEPT_PROPOSAL:
+        respond_to_accepted(msg); break;
+      case ACLMessage.REJECT_PROPOSAL:
+        System.out.println("Participant " + myAgent.getAID().getName() + ": Our proposal was rejected");
+        kill_service();
+        break;
+      default:
+        System.out.println("Participant " + myAgent.getAID().getName() + ": MSG type not recognised");
+        kill_service();
     }
   }
-}
-  
 
-  
+  protected void respond_to_cfp(ACLMessage msg) {
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Received CFP");
+    float dice = Math.random();
+
+    ACLMessage response = msg.createReply();
+
+    // Decide whether and what to respond
+    if (dice < 0.4) {
+      System.out.println("Participant " + myAgent.getAID().getName() + ": Decided to accept CFP");
+      response.setPerformative(ACLMessage.PROPOSE);
+    } else if (dice < 0.8) {
+      System.out.println("Participant " + myAgent.getAID().getName() + ": Decided to reject CFP");
+      response.setPerformative(ACLMessage.REJECT);
+    } else {
+      System.out.println("Participant " + myAgent.getAID().getName() + ": Decided to ignore CFP");
+      kill_service();
+      return;
+    }
+
+    // Set price and delay
+    int price = Math.ceil(Math.random() * 100);
+    int delay = Math.ceil(Math.random() * msg.getContent() * 1.5);
+
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Offering $" + toString(price) + ", after a delay of " + toString(delay));
+    response.setContent(price);
+    sleep(delay);
+
+    response.send();
+  }
+
+  protected void respond_to_accepted(ACLMessage msg) {
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Our proposal was accepted");
+
+    ACLMessage response = msg.createReply();
+    response.setPerformative(ACLMessage.INFORM);
+
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Working on project");
+    sleep(5000);
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Project done");
+    response.setContent("DONE");
+
+    response.send();
+    kill_service();
+  }
+
+  protected void kill_service() {
+    System.out.println("Participant " + myAgent.getAID().getName() + ": Exiting");
+    try { DFService.deregister(myAgent); } catch (FIPAException fe) { fe.printStackTrace(); }
+    myAgent.doDelete();
+  }
+}
