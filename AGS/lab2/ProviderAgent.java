7e2be204f19b16e7688d98e05334a2d025049bf1
Tomas Coufal <coufal.tom@gmail.com>
2018-04-11 15:39:00 +0200

    Cleanup AGS lab2 JADE

--- AGS/lab2/ProviderAgent.java
+++ AGS/lab2/ProviderAgent.java
@@ -1,4 +1,4 @@
-import jade.core.Agent; 
+import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -7,45 +7,45 @@ import jade.domain.FIPAException;
 import jade.domain.FIPANames;
 import jade.lang.acl.*;
 
-public class ProviderAgent extends Agent { 
+public class ProviderAgent extends Agent {
   protected void setup() {
     try {
-        DFAgentDescription dfd = new DFAgentDescription();
-        dfd.setName(getAID());
-        ServiceDescription sd = new ServiceDescription();
-        sd.setName(getAID().getName());
-  	sd.setType("ags-provider-type");
-        sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
-        dfd.addServices(sd);
-        DFService.register(this, dfd);
+      ServiceDescription sd = new ServiceDescription();
+      sd.setName(getAID().getName());
+      sd.setType("ags-provider-type");
+      sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+
+      DFAgentDescription dfd = new DFAgentDescription();
+      dfd.setName(getAID());
+      dfd.addServices(sd);
+
+      DFService.register(this, dfd);
     }
     catch (FIPAException fe) {
-        fe.printStackTrace();
+      fe.printStackTrace();
     }
-    
-    System.out.println("Provider " + getAID().getName() + " has created a service."); 
+
+    System.out.println("Provider " + getAID().getName() + " has created a service.");
     addBehaviour(new SubscribeAndWaitForMsgBehaviour());
-  } 
+  }
 }
 
 class SubscribeAndWaitForMsgBehaviour extends CyclicBehaviour{
   public void action() {
-    ACLMessage msg = myAgent.receive();  
-    if (msg == null){ 
+    ACLMessage msg = myAgent.receive();
+    if (msg == null){
       block();
+      return;
     }
-    else{
-      System.out.println("Provider " + myAgent.getAID().getName() + " received message. Service consumed.");
-      try {
-        DFService.deregister(myAgent);
-      }
-      catch (FIPAException fe) {
-        fe.printStackTrace();
-      }
-      myAgent.doDelete();
+
+    try {
+      DFService.deregister(myAgent);
     }
+    catch (FIPAException fe) {
+      fe.printStackTrace();
+    }
+
+    System.out.println("Provider " + myAgent.getAID().getName() + " received message. Service consumed.");
+    myAgent.doDelete();
   }
 }
-  
-
-  
--- AGS/lab2/ReceiverAgent.java
+++ AGS/lab2/ReceiverAgent.java
@@ -1,32 +1,29 @@
-import jade.core.Agent; 
+import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.lang.acl.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public class ReceiverAgent extends Agent { 
-  protected void setup() { 
-    System.out.println("Receiver agent " + getAID().getName() + " is ready."); 
+public class ReceiverAgent extends Agent {
+  protected void setup() {
+    System.out.println("Receiver agent " + getAID().getName() + " is ready.");
     addBehaviour(new ReceiveMsgBehaviour());
-  } 
+  }
 }
 
 
 class ReceiveMsgBehaviour extends CyclicBehaviour{
   private int counter = 0;
+
   public void action() {
-    ACLMessage msg = myAgent.receive();  
-    if (msg == null){ 
+    ACLMessage msg = myAgent.receive();
+    if (msg == null) {
       block();
-    }
-    else{
-      System.out.println(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()) + " | " + msg.getSender().getName() + " | "+  msg.getContent());
-      counter++;
-    }
-    if (counter >= 100)
-        myAgent.doDelete();
+      return;
+
+    System.out.println(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()) + " | " + msg.getSender().getName() + " | "+  msg.getContent());
+    counter++;
+
+    if (counter >= 100) myAgent.doDelete();
   }
 }
-  
-
-  
