40b8508aaa5a1d62497e5dab1f450a8f53dfcd8f
Tomas Coufal <coufal.tom@gmail.com>
2018-05-06 22:31:50 +0200

    BIN update and add presentation

--- BIN/multiplier.h
+++ BIN/multiplier.h
@@ -4,12 +4,13 @@
 #include "mul32x32_072.c"
 #include "mul32x32_092.c"
 
-inline double multiply(double a, double b) {
+inline float multiply(float a, float b) {
   uint32_t * a_ptr = reinterpret_cast<uint32_t*>(&a);
   uint32_t * b_ptr = reinterpret_cast<uint32_t*>(&b);
-  // return a*b;
-  // return (double) mul32x32_061(*a_ptr, *b_ptr);
-  // return (double) mul32x32_065(*a_ptr, *b_ptr);
-  // return (double) mul32x32_072(*a_ptr, *b_ptr);
-  return (double) mul32x32_092(*a_ptr, *b_ptr);
+  return a*b;
+  // uint64_t product = mul32x32_061(*a_ptr, *b_ptr);
+  // uint64_t product = mul32x32_065(*a_ptr, *b_ptr);
+  // uint64_t product = mul32x32_072(*a_ptr, *b_ptr);
+  // uint64_t product = mul32x32_092(*a_ptr, *b_ptr);
+  // return (float) product;
 }
